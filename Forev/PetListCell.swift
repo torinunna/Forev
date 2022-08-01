a072162fc9c59087cc555c9231db06b0f34bb465
torinunna <torinunna9588@gmail.com>
2022-08-01 18:45:33 +0900

    create PetList.storyboard, PetListVC, PetListCell

---
+++ Forev/PetListCell.swift
@@ -0,0 +1,15 @@
+//
+//  PetListCell.swift
+//  Forev
+//
+//  Created by 권유진 on 2022/08/01.
+//
+
+import UIKit
+
+class PetListCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
