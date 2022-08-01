0f647130bdf81383245f2f74d382e5776e55ae0a
torinunna <torinunna9588@gmail.com>
2022-08-02 02:16:49 +0900

    create SignUp.storyboard and SignUpVC

---
+++ Forev/SignUpViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewController.swift
+//  Forev
+//
+//  Created by 권유진 on 2022/08/02.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var idTextfield: UITextField!
+    @IBOutlet weak var pwTextfield: UITextField!
+    @IBOutlet weak var contactTextfield: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
